4f31ee844294a3c1363335fa93c7caa3a7978f44
DivyaMundhada <divyamundada9@gmail.com>
2021-06-21 19:20:01 +0530

    Repeated till Player reaches to winning position 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,33 +4,39 @@ public class SnakeAndLadder {
 	public static final int no_play = 0;
 	public static final int ladder = 1;
 	public static final int snake = 2;
+	public static final int win = 100;
 
 	public static void main(String[] args) {
 		int player_position = 0;
-		System.out.println("Welcome to Snake And Ladder Simulator.");
-		int option = (int) Math.floor((Math.random() * 10) % 3);
-		int roll_die = (int) Math.floor((Math.random() * 10) % 6) + 1;
+                System.out.println("Welcome to Snake And Ladder Simulator.");
 
-		switch (option) {
-		case no_play:
-			player_position += 0;
-			System.out.println("NO PLAY : You are at same position.");
-			break;
-		case ladder:
-			player_position += roll_die;
-			System.out.println("LADDER : You moved ahead by " + roll_die);
-			break;
-		case snake:
-			player_position -= roll_die;
-			System.out.println("SNAKE: You moved behind by " + roll_die);
-			if (player_position <= 0) {
-				player_position = 0;
-				System.out.println("Current position of the player in the game is: " + player_position);
+		while (player_position < win){
+
+			int option = (int) Math.floor((Math.random() * 10) % 3);
+			int roll_die = (int) Math.floor((Math.random() * 10) % 6) + 1;
+
+			switch (option) {
+			case no_play:
+				player_position += 0;
+				System.out.println("NO PLAY : You are at same position.");
+				break;
+			case ladder:
+				player_position += roll_die;
+				System.out.println("LADDER : You moved ahead by " + roll_die);
+				break;
+			case snake:
+				player_position -= roll_die;
+				System.out.println("SNAKE: You moved behind by " + roll_die);
+				if (player_position <= 0) {
+					player_position = 0;
+					System.out.println("Current position of the player in the game is: " + player_position);
+				}
+				break;
+			default:
+				System.out.println("Error occured");
+				break;
 			}
-			break;
-		default:
-			System.out.println("Error occured");
-			break;
 		}
+		System.out.println("You WIN at " + player_position);
 	}
 }
