e65154e430855dcfd3aa8ad6bf5b46157a86eaff
DivyaMundhada <divyamundada9@gmail.com>
2021-06-21 10:11:21 +0530

    Snake and Ladder Game Started.

---
+++ SnakeAndLadder.java
@@ -0,0 +1,9 @@
+public class SnakeAndLadder {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake And Ladder Simulator.");
+
+		int player_position = 0;
+		System.out.println("Your initial position is : " + player_position);
+
+	}
+}
