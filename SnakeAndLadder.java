5d734cda97c5876e293347e83b759c208ed391d3
DivyaMundhada <divyamundada9@gmail.com>
2021-06-21 10:50:27 +0530

    Die is Rolled to get a number between 1 to 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,9 +1,9 @@
 public class SnakeAndLadder {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Snake And Ladder Simulator.");
+public static void main(String[] args){
+    System.out.println("Welcome to Snake And Ladder Simulator.");
 
-		int player_position = 0;
-		System.out.println("Your initial position is : " + player_position);
-
-	}
+    int player = 0;
+    int roll_die = (int)Math.floor((Math.random() * 10) % 6) + 1;
+    System.out.println("Rolled: " + roll_die);
+}
 }
